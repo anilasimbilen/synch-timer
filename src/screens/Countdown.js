aa35084c1eada2df8e05982356e2766c65d2642f
Anıl Asım Bilen <abilen18@ku.edu.tr>
2020-03-27 14:30:39 +0300

    First Blood

---
+++ src/screens/Countdown.js
@@ -0,0 +1,121 @@
+import React, {Component} from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import styled from "styled-components";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
+import InputGroup from "react-bootstrap/InputGroup";
+
+const Styles = styled.div 
+`
+    h1 {
+        padding: 4vh;
+        color: #fff;
+        font-size: calc(3vw + 70px);
+    }
+    #middle {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+`
+
+export default class Countdown extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hrs: Number(this.props.hours),
+            mins: Number(this.props.minutes),
+            secs: Number(this.props.seconds),
+            isStarted: this.props.starting !== "f",
+            startingTime:  this.props.starting === "f" ? null : Number(this.props.starting),
+            background: [52,196,47],
+            inits: {hrs: Number(this.props.hours),
+                mins: Number(this.props.minutes),
+                secs: Number(this.props.seconds)}
+        }
+    }
+    componentDidMount() {
+        console.log(this.state);
+        console.log(this.state.background.join(","));
+        if(this.state.isStarted) {
+            var c = this.durationToMillis(this.state.inits);
+            var dif = this.state.startingTime - Date.now();
+            console.log(dif);
+            this.setState(this.millisToDuration(c + dif));
+            this.handleStart(true);
+        }
+    }
+    handleStart = (bypass) => {
+        clearInterval(this.timer);
+        if(!bypass && this.state.isStarted) {
+            return;
+        }
+        if(!this.state.isStarted) {
+            this.setState({
+                isStarted: true,
+                startingTime: Date.now()
+            });
+        }
+        
+        this.timer = setInterval(() => {
+            if(this.durationToMillis(this.state) === 0) {
+                clearInterval(this.timer);
+                return;
+            }
+            var c = this.durationToMillis(this.state.inits);
+            var dif = this.state.startingTime - Date.now();
+            this.setState(this.millisToDuration(c + dif));
+        }, 100);
+    }
+    durationToMillis = ({hrs, mins, secs}) => {
+        return hrs * 3600000 + mins * 60000 + 1000 * secs;
+    }
+    millisToDuration = (m) => {
+        let k = m;
+        let hrs = Math.floor(k / 3600000);
+        k %= 3600000;
+        let mins = Math.floor((k) / 60000);
+        k %= 60000;
+        let secs = Math.floor(k / 1000);
+        return ({hrs, mins, secs});
+    }
+    render() {
+        return(
+            <Styles style={{
+                width: "100vw",
+                height: "100vh",
+                backgroundColor: `rgba(${this.state.background.join(",")})`,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+            }}>
+                <Row style={{margin: 0, justifyContent: "center"}}>
+                    <Col id="middle">
+                        <div style={{width: "100%"}}>
+                        <h1>
+                            {this.state.hrs} : {this.state.mins} : {this.state.secs}
+                        </h1>
+                        </div>
+                    <div style={{width: "100%", justifyContent: "space-around", alignItems: "center", margin: 0, flexDirection: "column", display: "flex"}}>
+                             {
+                                 !this.state.isStarted ? <Button size="lg" style={{width: "50%", margin: 20}} onClick={e => this.handleStart(false)}>
+                                 Start
+                             </Button> : 
+                            <InputGroup>
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text>
+                                        Share
+                                    </InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl value={window.location.toString().replace("f", this.state.startingTime)}/>
+                            </InputGroup>
+                             }
+                        </div>
+                    </Col>
+                </Row>
+            </Styles>
+        );
+    }
+}
---
+++ src/screens/CountdownPicker.js
@@ -0,0 +1,168 @@
+import React, {Component} from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import styled from "styled-components";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import { Redirect } from "react-router-dom";
+
+const Styles = styled.div`
+
+    #middle {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    h1 {
+        padding: 4vh;
+        color: #fff;
+        font-size: calc(3vw + 70px);
+    }
+    .changeButton {
+        :hover {
+            background-color: #ced4da;
+        }
+    }
+`
+
+export default class CountdownPicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            initialTime: props.initialTime,
+            current: Date.now(),
+            unit: null,
+            title: "Minutes",
+            renderingUnitPicker: false,
+            duration: [0,0,0],
+            redir: ""
+        }
+    }
+
+    componentDidMount() {
+        
+    }
+    renderTimePicker = () => {
+        return(
+            <InputGroup size="sm" className="mb-3">
+            <FormControl aria-label="Hours" onChange={e => {
+                e.preventDefault();
+                let mk = e.target.value;
+                if(mk.length === 0){ 
+                    mk = "0";
+                }
+                try {
+                    let nn = parseInt(mk);
+                    if(nn >= 0) {
+                        let duration = this.state.duration;
+                        duration[0] = nn;
+                        this.setState({
+                            duration
+                        })
+                    }
+                } catch(err) {
+                    
+                }
+            }} value={this.state.duration[0]} />
+            <InputGroup.Append>
+            <InputGroup.Text>
+                Hours
+              </InputGroup.Text>
+            </InputGroup.Append>
+            <FormControl aria-label="Minutes" onChange={e => {
+                e.preventDefault();
+                let mk = e.target.value;
+                if(mk.length === 0){ 
+                    mk = "0";
+                }
+                try {
+                    let nn = parseInt(mk);
+                    if(nn >= 0) {
+                        let duration = this.state.duration;
+                        duration[1] = nn;
+                        this.setState({
+                            duration
+                        })
+                    }
+                } catch(err) {
+                    
+                }
+            }} value={this.state.duration[1]} />
+            <InputGroup.Append>
+            <InputGroup.Text>
+                Minutes
+              </InputGroup.Text>
+            </InputGroup.Append>
+            <FormControl aria-label="Seconds" onChange={e => {
+                e.preventDefault();
+                let mk = e.target.value;
+                if(mk.length === 0){ 
+                    mk = "0";
+                }
+                try {
+                    let nn = parseInt(mk);
+                    if(nn >= 0) {
+                        let duration = this.state.duration;
+                        duration[2] = nn;
+                        this.setState({
+                            duration
+                        })
+                    }
+                } catch(err) {
+                    
+                }
+            }} value={this.state.duration[2]} />
+            <InputGroup.Append>
+            <InputGroup.Text>
+            Seconds
+              </InputGroup.Text>
+            </InputGroup.Append>
+            
+          </InputGroup>
+        
+        );
+    }
+    handleDone = () => {
+        let {duration} = this.state;
+        this.setState({
+            redir: `/timer/${duration[0]}/${duration[1]}/${duration[2]}/f`
+        })
+    }
+    render() {
+        if(this.state.redir !== "") {
+            return <Redirect to={this.state.redir} />;
+        }
+        return(
+            <Styles style={{
+                width: "100vw",
+                height: "100vh",
+                backgroundColor: "rgba(52, 196, 47)",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+            }}>
+                <Row style={{margin: 0, justifyContent: "center"}}>
+                    <Col id="middle" md={12} xs={10}>
+                        <h1>
+                            Synch-Time Picker
+                        </h1>
+                        <Row>
+                            {this.renderTimePicker()}
+                        </Row>
+                        <Row>
+                                <Button variant="light" size="lg" onClick={this.handleDone}>
+                                    Done
+                                </Button>
+                        </Row>
+                    </Col>
+                </Row>
+            </Styles>
+
+        );
+    }
+}
